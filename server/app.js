062f1a5c605144dbc6c039fd7f01b4afff6ce094
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-08-28 08:51:52 +0800

    first commit

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+// allow cross-origin requests
+app.use(cors());
+
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect(
+	`mongodb+srv://andrewcbuensalida:${process.env.DB_PASSWORD}@graphql-net-ninja-books.iirvr.mongodb.net/graphql-net-ninja-books-db?retryWrites=true&w=majority`
+);
+mongoose.connection.once("open", () => {
+	console.log("conneted to database");
+});
+
+// bind express with graphql
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema,
+		graphiql: true,
+	})
+);
+
+app.listen(4000, () => {
+	console.log("now listening for requests on port 4000");
+});
