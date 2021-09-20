1d426ddc1974cbe65fc95743c5936ad3a266ab0c
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-19 19:58:33 -0700

    l

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,8 @@ import AddBook from "./components/AddBook";
 // apollo client setup
 const client = new ApolloClient({
 	// uri: "https://54.169.202.39:4000/graphql", //public ip
-	uri: "https://www.books.anhonestobserver.com:4000/graphql",
-	// uri: "https://172.31.24.80:4000/graphql", //private ip
+	// uri: "https://www.books.anhonestobserver.com:4000/graphql",
+	uri: "http://172.31.24.80:4000/graphql", //private ip
 	// uri: "http://localhost:4000/graphql",
 });
 
