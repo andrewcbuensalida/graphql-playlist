4667e9bda1348e58d58ee0dfc69dbc6503e631cb
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-10-11 01:50:24 -0700

    books

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -9,7 +9,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #navigate into our working directory where we have all our github files
-cd /home/ubuntu/express-app/server
+cd /home/ubuntu/books/server
 
 #install node modules
 npm ci --production --prefer-offline
